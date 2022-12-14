8ee5964b0c4523a5e0cef490019f1b0e36437535
BrendonSmartGuy <115043433+BrendonSmartGuy@users.noreply.github.com>
2022-12-14 17:05:38 -0600

    2022

---
+++ Text-Editor.js
@@ -0,0 +1,4 @@
+//enable
+javascript:document.body.contentEditable = 'true'; document.designMode='on'; void 0
+//disable
+javascript:document.body.contentEditable = 'false'; document.designMode='off'; void 100
